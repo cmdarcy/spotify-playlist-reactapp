c525ba1d8382baeb4b3c1a6741547b55b91e8dd5
Christopher d'Arcy <cdarcy656481@gmail.com>
2024-02-29 11:06:04 -0500

    build basic track, tracklist components

---
+++ src/components/track/track.jsx
@@ -0,0 +1,8 @@
+export default function Track() {
+	return (
+		<>
+			<h1>Track Title</h1>
+			<h2>Artist Name</h2>
+		</>
+	);
+}
---
+++ src/components/tracklist/tracklist.jsx
@@ -0,0 +1,11 @@
+import Track from "../track/track";
+
+export default function Tracklist() {
+	return (
+		<ul>
+			<Track />
+			<Track />
+			<Track />
+		</ul>
+	);
+}
