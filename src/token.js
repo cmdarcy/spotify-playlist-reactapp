6c0c2676ffd2aeb1afb3eaf9d694fbbb5c8a5cfc
Christopher d'Arcy <cdarcy656481@gmail.com>
2024-03-26 09:21:27 -0400

    update redirect uri in token logic

--- src/token.js
+++ src/token.js
@@ -1,5 +1,5 @@
 const client_id = "66ca34498a6142318ea76ae6f763cc0f";
-const redirect_uri = "http://localhost:3000";
+const redirect_uri = "https://spotifyplaylistappreactproject.netlify.app/";
 const scope = "playlist-modify-public playlist-modify-private";
 
 let url = "https://accounts.spotify.com/authorize";
